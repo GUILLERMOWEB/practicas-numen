5ed3b85cad45dc6dfac7739891a0c3fac99e1d7b
Guillermo <ghgenet@yahoo.com.ar>
2022-05-27 23:12:26 -0300

    inicie mi repositorio

---
+++ Java Script/sandbox.js
@@ -0,0 +1,16 @@
+let per1= "Maira"
+let per2= "Guille"
+let per3= "Mile"
+let per4= "Euge"
+let edadper1="46"
+let edadper2="44"
+
+console.log (per1)
+console.log (per3)
+console.log (per1,edadper1,per2,edadper2)
+
+console.log (per4,per1)
+let x= 5
+let y= 6
+
+console.log ((x*y)*(x*y)+1000)
